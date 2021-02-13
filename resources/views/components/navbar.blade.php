57f9b456d59a6d653091adaa3dfc7e18667b5cfe
Justin Labak <jtlabak@gmail.com>
2021-02-13 00:59:54 -0800

    some navbar improvements

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
       </div>
     </section>
 
-    <section class="col-span-7">
+    <section class="col-span-6">
      <div class="text-gray-600 relative">
         <input class="border border-gray-300 bg-white w-full h-10 text-lg px-9 rounded focus:outline-none font-medium"
           type="search" name="search" placeholder="Search">
@@ -17,7 +17,7 @@
       </div>
     </section>
 
-    <section class="col-span-3 flex flex-row-reverse">
+    <section class="col-span-4 flex flex-row-reverse">
       <div class="flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
         <button class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
           <span class="sr-only">View notifications</span>
@@ -31,7 +31,7 @@
         <div>
         <button @click="open = !open" class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true" x-bind:aria-expanded="open">
           <span class="sr-only">Open user menu</span>
-          <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+          <img class="h-8 w-8 rounded-full" src="{{ Auth::user()->picture_url }}" alt="">
         </button>
         </div>
         <div x-show="open" x-description="Profile dropdown panel, show/hide based on dropdown state." x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu" style="display: none;">
